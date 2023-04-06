1d1f8ec58b408354272473ab9edb2fa72fbcccfc
ukizet <ukikatuki@gmail.com>
2023-04-07 02:55:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# centr_zainyatosti_bot
+
+This is a telegram bot that allows you to view, add, delete and modify vacancies in the database
+
+Bot is running through "launch_bot.py"
---
+++ run_telegram_bot_centr_zainyatosti(example).bat
@@ -0,0 +1,14 @@
+@echo off
+
+set TOKEN=
+
+cd C:\progs_games_shortcuts\LearnPython4\centr_zainyatosti_bot
+
+cd
+
+call ..\.venv\Scripts\activate.bat
+
+python launch_bot.py
+
+pause
+
