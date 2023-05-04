88a69f5b44360d939ddd6331b193b371efa85132
ukizet <ukikatuki@gmail.com>
2023-05-04 13:59:53 +0300

    update

--- handlers/client/classes/menu_handlers.py
+++ handlers/client/classes/menu_handlers.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 from aiogram import types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils import exceptions
 from create import bot, db
 
 
@@ -33,7 +32,7 @@ class MenuHandlers:
         self.messages_id = []
         self.chat_id = 0
         self.vacs_message = ''
-        
+
     async def display_menu(self, message: types.Message = None,
                            callbackQuery: types.CallbackQuery = None):
         """Цей хендлер відповідає за відображення меню вакансій.
@@ -56,18 +55,20 @@ class MenuHandlers:
                  InlineKeyboardButton(text="▶️",
                                       callback_data="next"))
 
-        self.vacs_list = self.all_vacancies[self.previous_page * self.VACANCIES_PER_PAGE: self.page * self.VACANCIES_PER_PAGE]
+        self.vacs_list = self.all_vacancies[
+            self.previous_page * self.VACANCIES_PER_PAGE:
+            self.page * self.VACANCIES_PER_PAGE]
 
         if callbackQuery is None:
             self.vacs_message = ''
             for i in range(len(self.vacs_list)):
                 vacancy_info = (
-                               f"Назва вакансії: {self.vacs_list[i][2]}\n"
-                               f"Опис: {self.vacs_list[i][3]}\n"
-                               f"ЗП: {self.vacs_list[i][4]}\n"
-                               "\n"
-                               )
-                
+                    f"Назва вакансії: {self.vacs_list[i][2]}\n"
+                    f"Опис: {self.vacs_list[i][3]}\n"
+                    f"ЗП: {self.vacs_list[i][4]}\n"
+                    "\n"
+                )
+
                 if i == len(self.vacs_list) - 1:
                     self.vacs_message += vacancy_info
                     test_message = await message.answer(self.vacs_message,
@@ -75,31 +76,30 @@ class MenuHandlers:
                     self.messages_id.append(test_message.message_id)
                 else:
                     self.vacs_message += vacancy_info
-                
+
         else:
             self.vacs_message = ''
             for i in range(len(self.vacs_list)):
                 vacancy_info = (
-                               f"Назва вакансії: {self.vacs_list[i][2]}\n"
-                               f"Опис: {self.vacs_list[i][3]}\n"
-                               f"ЗП: {self.vacs_list[i][4]}\n"
-                               "\n"
-                               )
+                    f"Назва вакансії: {self.vacs_list[i][2]}\n"
+                    f"Опис: {self.vacs_list[i][3]}\n"
+                    f"ЗП: {self.vacs_list[i][4]}\n"
+                    "\n"
+                )
 
                 self.vacs_message += vacancy_info
             await bot.edit_message_text(chat_id=callbackQuery.message.chat.id,
                                         message_id=self.messages_id[0],
                                         text=self.vacs_message,
                                         reply_markup=self.inline_kb)
-            print()
 
     async def inline_button_back(self, callbackQuery: types.CallbackQuery):
         """Back button handler"""
         if len(self.all_vacancies) <= self.VACANCIES_PER_PAGE:
-            callbackQuery.answer("Немає попередніх вакансій")
+            await callbackQuery.answer("Немає попередніх вакансій")
             return
         if self.page <= 1:
-            callbackQuery.answer("Немає попередніх вакансій")
+            await callbackQuery.answer("Немає попередніх вакансій")
             return
         self.previous_page = self.page-2
         self.page -= 1
@@ -111,7 +111,7 @@ class MenuHandlers:
         # зробити так щоб ця кнопка змінювала ці 5 вакансій на наступні 5 вакансій
 
         if len(self.all_vacancies) <= self.VACANCIES_PER_PAGE:
-            callbackQuery.answer("Немає наступних вакансій")
+            await callbackQuery.answer("Немає наступних вакансій")
             return
         self.previous_page = self.page
         self.page += 1
